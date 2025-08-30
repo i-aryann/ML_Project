a169e43ed2135f6701548ddf9968c2a831c4945d
i-aryann <aryangupta.7263@gmail.com>
2025-08-30 23:36:28 +0530

    Adding Exception Handling

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
 from source.ml_project.logger import logging
+from source.ml_project.exception import CustomException
+import sys
+
+
 
 if __name__=='__main__':
     logging.info("Logging has started")
+
+
+
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Custom Exception")
+        raise CustomException(e,sys)
 
